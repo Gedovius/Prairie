3755f57c756fa6e2bbf9a8b1910839474be95bd6
gedovius <annegedovius@hotmail.fr>
2020-03-30 15:56:48 +0200

    j'ai enfin réussi à envoyer cette grosse merdasse

--- controller/insertion.php
+++ controller/insertion.php
@@ -1,5 +1,6 @@
 <?php
-var_dump($_POST);
+
+//var_dump($_POST);
 $username = $_POST['username'];
 $email = $_POST['email'];
 $keypass = $_POST['keypass'];
@@ -16,14 +17,8 @@ $sql = "INSERT INTO user (user_name, user_email, user_keypass) VALUES (?,?,?)";
 $preparation = $pdo->prepare($sql);
 $preparation->bindParam(1, $username);
 $preparation->bindParam(2, $email);
-$preparation->bindParam(3, keypass);
+$preparation->bindParam(3, $keypass);
 $preparation->execute();
-
-
-
-
-
-
 
 
 
