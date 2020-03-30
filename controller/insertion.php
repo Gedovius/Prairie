5958beee6b7d9a0bd1da95997d6a63e63494f439
gedovius <annegedovius@hotmail.fr>
2020-03-30 15:44:58 +0200

    gedovius

---
+++ controller/insertion.php
@@ -0,0 +1,30 @@
+<?php
+var_dump($_POST);
+$username = $_POST['username'];
+$email = $_POST['email'];
+$keypass = $_POST['keypass'];
+
+// se connecter à la BDD
+try {
+
+    $pdo = new PDO('mysql:host=localhost;dbname-atelier_prairie','gedovius','Gedo1990%');
+} catch (PDOException $e) {
+    echo 'connexion échouée';
+}
+// insérer la ligne utilisateur en BDD
+$sql = "INSERT INTO user (user_name, user_email, user_keypass) VALUES (?,?,?)";
+$preparation = $pdo->prepare($sql);
+$preparation->bindParam(1, $username);
+$preparation->bindParam(2, $email);
+$preparation->bindParam(3, keypass);
+$preparation->execute();
+
+
+
+
+
+
+
+
+
+
